e36886e1c40847f6ff1575100818cde218e053e4
PoymaLana <marikakopina@gmail.com>
2020-12-25 10:47:48 +0300

    4.1.2. Глупый бот [#437137]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl =  "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        } else {
+            System.out.println("Это ставит меня в тупик. Задайте другой вопрос.");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Который час?");
+        System.out.println(rsl);
+    }
+}
