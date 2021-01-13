f1504eb7bd67043904fd5ec678baabb1be07574c
PoymaLana <marikakopina@gmail.com>
2021-01-13 18:14:50 +0300

    6.4.1.1. Обход массива с последнего элемента [#437188]

--- src/ru/job4j/array/Reverse.java
+++ src/ru/job4j/array/Reverse.java
@@ -4,7 +4,7 @@ public class Reverse {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7};
         for (int i = 0; i < numbers.length; i++) {
-            if (i % 2 == 0 && i != 6) {
+            if (i % 2 == 0 && i != numbers.length-1) {
                 System.out.println(
                         "Текущий элемент массива начиная с последнего: "
                                 + numbers[numbers.length - 1 - i]
