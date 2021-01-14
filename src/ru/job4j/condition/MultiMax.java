b2d3a347f143f69f62849c6f17f47ba45ac1bd25
PoymaLana <marikakopina@gmail.com>
2021-01-14 11:00:28 +0300

    4.4. Вложенный оператор if else [#437155]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -3,34 +3,17 @@ package ru.job4j.condition;
 public class MultiMax {
 
     public static int max(int first, int second, int third) {
-        if (first > second && first > third) {
+        if (first >= second && first >= third) {
             return first;
-        } else if (second > first && second > third) {
+        } else if (second >= first && second >= third) {
             return second;
-        } else {
-            return third;
         }
+        return third;
     }
-
-    public static int maxAnother(int first, int second, int third) {
-        if (first > second) {
-            if (first > third) {
-                return first;
-            } else {
-                return third;
-            }
-            }  else if (second > third) {
-            return second;
-            } else {
-            return third;
-        }
-    }
-
-
 
     public static void main(String[] args) {
         System.out.println(MultiMax.max(10, 10, 1));
-        System.out.println(MultiMax.maxAnother(10, 10, 1));
+
     }
 }
 
