570ed9b2508383de718727a69bb269a65cef7f56
PoymaLana <marikakopina@gmail.com>
2021-01-18 13:45:42 +0300

    1. Экзамен. Базовый синтаксис. [#437238]

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -30,13 +30,4 @@ public class Merge {
     }
 }
 
-    /*public static int[] merge(int[] left, int[] right) {
-        int[] rsl = new int[left.length + right.length];
-        for (int index = 0; index < left.length; index++)  {
-            rsl[index] = left[index];
-        }
-        for (int index = 0; index < right.length; index++) {
-            rsl[index + left.length] = right[index];
-        }
-        return rsl;
-    }*/
+
